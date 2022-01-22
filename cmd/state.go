adbaf1b1841bbfc0b05a8898eaeef96f69b29d4b
Bryan Bess <1250248+squarejaw@users.noreply.github.com>
2022-01-22 10:47:43 -0600

    Add state command

---
+++ cmd/state.go
@@ -0,0 +1,49 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	"github.com/squarejaw/wiz/udp"
+	"github.com/tidwall/gjson"
+)
+
+// stateCmd represents the state command
+var stateCmd = &cobra.Command{
+	Use:   "state",
+	Short: "Get current bulb state",
+	Long:  `Sends a UDP request to get the current state of the bulb and prints it to standard out`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			cobra.CheckErr(fmt.Errorf("on needs an IP address of a bulb"))
+		}
+		ip := args[0]
+		udpSession, err := udp.NewSession(ip+":38899", time.Duration(timeout)*time.Second)
+		cobra.CheckErr(err)
+		defer udpSession.Close()
+
+		msg := []byte(`{"method":"getPilot","params":{}}`)
+		_, err = udpSession.Write(msg)
+		cobra.CheckErr(err)
+
+		buf := make([]byte, 1024)
+		_, _, err = udpSession.Read(buf)
+		if errors.Is(err, os.ErrDeadlineExceeded) {
+			return
+		}
+		cobra.CheckErr(err)
+		result := gjson.GetBytes(buf, "result")
+		fmt.Println(result)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(stateCmd)
+}
